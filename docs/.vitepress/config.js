218609526cdd541a3f7f6686f369095def7f8489
Himanshu-AT <aristech.official@gmail.com>
2024-02-12 22:00:19 +0530

    make website, docs

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    title: "RusCode Docs",
+    description: "Rus Code is an VS Code Open Source alternative written in Rust and NextJS",
+    themeConfig: {
+        nav: [
+            { text: "Home", link: "/" },
+            { text: "initial github", link: "https://github.com/Himasnhu-AT/ruscode.git" }
+        ],
+        sidebar: [
+            {
+                text: "Getting Started",
+                items: [
+                    { text: "Home", link: "/" },
+                ]
+            }
+        ]
+    }
+};
---
+++ docs/index.md
@@ -0,0 +1,3 @@
+::: warning
+This project is still in development and is not ready for production use. The documentation is also a work in progress.
+:::
